2f5a7861fd85435ea1376ed339ab91f1ab6a7b69
Modhura <32320483+modhura1@users.noreply.github.com>
2024-03-04 20:25:23 +0530

    Create The_Hurdle_Race.js

---
+++ Hackerrank/The_Hurdle_Race.js
@@ -0,0 +1,66 @@
+// Problem : https://www.hackerrank.com/challenges/the-hurdle-race/problem?isFullScreen=true 
+
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+function hurdleRace(n, k, height) {
+    // Write your code here
+    height.sort(function(a,b) { return a - b; });
+    console.log(height)
+    let h = height[n-1];
+    if (h > k) {
+        return h-k;
+    } else 
+        return 0;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const height = readLine().replace(/\s+$/g, '').split(' ').map(heightTemp => parseInt(heightTemp, 10));
+
+    const result = hurdleRace(n, k, height);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
